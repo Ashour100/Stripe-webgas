77f4b6260ead935610de37046c840290610b62b4
Ashour100 <interinstitute5@gmail.com>
2022-12-07 15:51:57 +0100

    Payment form configurations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/customer',CustomerController::class);
+    Route::resource('/form',FormController::class);
+    // Route::get('/customer/:stripe_id',[CustomerController::class,'show']);
 });
 
 require __DIR__.'/auth.php';
